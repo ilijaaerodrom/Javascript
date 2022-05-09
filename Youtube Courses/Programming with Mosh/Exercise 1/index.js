b9c21267247fb863744fa321d5f313139275be1d
ilijaaerodrom <ilija_trajkovski@outlook.com>
2022-05-09 13:41:52 +0200

    learning

--- Youtube Courses/Programming with Mosh/Exercise 1/index.js
+++ Youtube Courses/Programming with Mosh/Exercise 1/index.js
@@ -21,9 +21,52 @@ console.log(interestRate);
 // Primitives / Value Types
 // String, Number, Boolean, undefined and null
 
-let name = 'Ilija'; // String
+let name1 = 'Ilija'; // String
 let age = 30; // Number
 let isApproved = true; // Boolean
-let firstName = undefined;
+let firstName1 = undefined;
 let selectedColor = null;
+
+// Refrence Types
+// Object, Array and Function
+
+// Object
+let name2 = `Ilija`;
+let age1 = `35`;
+let person = {
+  name2: `Ilija`,
+  age1: 35
+};
+
+// Dot Notation
+person.name2 = `Ronaldo`;
+// Bracket Notation
+person[`name2`] = `Il Fenomeno`;
+
+console.log(person.name2);
+
+// Arrays
+let selectedColors = [`red`, `blue`];
+selectedColors[2] = `green`;
+console.log(selectedColors.length);
+
+// Functions
+// Perfoming a task
+function greet(name, lastName) {
+  console.log(`Hello ` + name + ` ` + lastName);
+}
+greet(`Tramp`, `Donald`);
+
+// Calculating a value
+function square(number) {
+  return number * number;
+}
+console.log(square(2));
+
+
+
+
+
+
+
 
