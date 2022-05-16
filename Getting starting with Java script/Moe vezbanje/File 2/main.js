df0744f9125fe315266c3b236afb7a34c3292f44
ilijaaerodrom <ilija_trajkovski@outlook.com>
2022-05-16 08:13:45 +0200

    push

---
+++ Getting starting with Java script/Moe vezbanje/File 2/main.js
@@ -0,0 +1,19 @@
+
+/* Arrays are type of object, which
+allows you ti store multiple values. 
+These values can be of all data types
+and one array can eve contain different
+data types. */
+
+arr1 = [`purple`, `orange`, `green`];
+console.log(arr1);
+
+// Array with 10 undefined values
+arr2 = new Array(10);
+console.log(arr2);
+
+// Array with one value of 10
+arr3 = [10];
+console.log([10]);
+
+
