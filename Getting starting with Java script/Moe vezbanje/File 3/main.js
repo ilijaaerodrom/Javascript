55a2a0c0ae72ce23c96433291d004611cb97862e
ilijaaerodrom <ilija_trajkovski@outlook.com>
2022-05-17 11:39:38 +0200

    finish prezentation

--- Getting starting with Java script/Moe vezbanje/File 3/main.js
+++ Getting starting with Java script/Moe vezbanje/File 3/main.js
@@ -37,7 +37,10 @@ clubs.splice(1,3);
 console.log(clubs);
 let newCars = [`toyota`, `passat`, `bmw`, `honda`, `ford`];
 console.log(newCars);
-newCars
+newCars.splice(2,2);
+console.log(newCars);
+newCars.sort();
+console.log(newCars);
 
 
 // concat
@@ -59,3 +62,162 @@ clubs.shift();
 console.log(clubs);
 shoppingList.shift();
 console.log(shoppingList);
+
+// Multidimensional Arrays
+let mulArr1 = [1, 2, 3];
+let mularr2 = [4, 5, 6];
+let mulArr3 = [5, 6, 7];
+let allMulArr = [mulArr1, mularr2, mulArr3];
+console.log(allMulArr);
+let allMulArr1 = [[mulArr1, mularr2, mulArr3]];
+console.log(allMulArr1);
+let someArr = allMulArr[1,1];
+console.log(someArr);
+
+//Vezba
+let valArr1 = [1, 2, 3];
+let valArr2 = [valArr1, valArr1, valArr1];
+console.log(valArr2);
+
+// Objects can group multiple variables into one
+let dog = {
+  dogName: `Reks`,
+  weight: 40,
+  color: `white`,
+  breed: `pudle`,
+  age: 5,
+  burglarBiter: true
+};
+console.log(dog);
+let dogColor1 = dog.color;
+console.log(dogColor1);
+
+// Updating Objects
+dog.breed = `Husky`;
+dog[`weight`] = 35;
+console.log(dog);
+dog.color = `brown-black`;
+console.log(dog);
+dog[`age`] = 8;
+let newAge = `age`;
+console.log(dog[newAge]);
+
+// Vezba
+myCar = {
+  make: `folcwagen`,
+  model: `passat`,
+  year: `2008`,
+  color: `silver`,
+  doors: 5,
+  manual: true
+};
+let newColor = `color`;
+myCar[newColor] = `red`;
+console.log(myCar[newColor]);
+newColor = `forSale`;
+myCar[newColor] = true;
+console.log(myCar[newColor]);
+console.log(myCar.make + ` ` + myCar.model);
+console.log(`forSale ` +  myCar[newColor]);
+
+// Objects in Objects
+let company = {
+  companyName: `Healty Candy`,
+  activities: [`food manufactoring`, `improving kids health`,
+              `manufactoring toys`],
+    address: {
+      street: `2nd street`,
+      number: `123`,
+      zipcode: `33116`,
+      city: `Miami`,
+      state: `Florida`
+    },
+    yearOfEstamblishment: 2001          
+};
+  company.address.zipcode = `33117`;
+  console.log(company);
+  company[`address`][`city`] = `San Jose`;
+  console.log(company[`address`][`city`]);
+  let activity = company.activities[1];
+  console.log(activity);
+
+  // Objects in Arrays
+  // Objects in Arrays in Objects
+
+  // Vezba
+
+  let people = {
+    friends: [],
+  };
+
+  let Bojan = {
+    firstName: `Bojco`,
+    lastName: `Popovski`,
+    value: `34`
+  }
+
+  let Davor = {
+    firstName: `Davor`,
+    lastName: `Rashic`,
+    value: `35`
+  }
+
+  let Zoki = {
+    firstName: `Zoran`,
+    lastName: `Ognanoski`,
+    value: `35`
+  }
+
+   people.friends.push(`Bojan`);
+   console.log(Bojan);
+   people.friends.push(`Davor`);
+   console.log(Davor);
+   people.friends.push(`Zoki`);
+   console.log(Zoki);
+   
+  // Vezba
+
+const theList = [`Laurence`, `Svekis`, true, 35, null, 
+undefined, {test: `one`, score: 55}, [`one`, `two`]];
+console.log(theList);
+theList.shift();
+theList.pop();
+console.log(theList);
+theList.unshift(`FIRST`);
+console.log(theList);
+theList.splice(3,4, `hello world`);
+console.log(theList);
+let index = theList.indexOf(true);
+theList[index] = `MIDDLE`;
+console.log(theList);
+theList.push(`LAST`);
+console.log(theList);
+
+// Vezba - Company Product Catalog
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Self - Check Quiz
+const myArr1 = [1,3,5,6,8,9,15];
+console.log(myArr1);
+myArr1[1] = 4;
+console.log(myArr1);
+console.log(myArr1.indexOf(0));
+console.log(myArr1.indexOf(3));
+
+
