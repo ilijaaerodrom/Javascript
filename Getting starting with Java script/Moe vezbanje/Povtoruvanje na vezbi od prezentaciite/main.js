889e5a0fe9c06e7ef188619c36788e396e163bdd
ilijaaerodrom <ilija_trajkovski@outlook.com>
2022-05-26 12:48:21 +0200

    index and javascript

---
+++ Getting starting with Java script/Moe vezbanje/Povtoruvanje na vezbi od prezentaciite/main.js
@@ -0,0 +1,64 @@
+
+
+// Chapter 1: Getting started with Javascript
+// Exercise 1.1
+console.log(4 + 10);
+console.log(`Ilija Trajkovski`);
+
+// Exercise 1.2
+console.log(`hello world`);
+
+// Exercise 1.3
+// It is linked and it is working
+
+// Exercise 1.4
+let a = 10; // value of 10
+console.log(a);
+/* multi line
+comment in
+Javascript */
+
+// Project 1.1
+/* let myName = `Ilija Trajkovski`;
+multi line comment
+my code */
+
+// Chapter 2: Javascript Essentials
+// Exercise 2.1
+let str1 = 'Laurence';
+console.log(typeof str1);
+let str2 = "Svekis";
+console.log(typeof str2);
+let val1 = undefined;
+console.log(typeof val1);
+let val2 = null;
+console.log(typeof val2);
+let myNum = 1000;
+console.log(typeof myNum);
+
+// Exercise 2.2
+let firstName = `Ilija`;
+let age = `35`;
+let bool = true;
+console.log(`Hello, my name is ${firstName}, I am ${age} years old and 
+I can code JavaScript: ${bool}`);
+
+// Exercise 2.3
+// Pythagorean theorem
+
+// Exercise 2.4
+let num1 = 5;
+let num2 = 10;
+let num3 = 15;
+console.log(num1 + num2);
+console.log(num1 / num3);
+let num4 = num3 % num2;
+console.log(`${num3} % ${num2} = ${num4}`);
+
+// Project 2.1
+let km = 130;
+const conversion = 1.60934;
+let miles = km * conversion;
+console.log(`The distance of ${km} kms is euqal to ${miles} mil.`);
+
+// Project 2.2
