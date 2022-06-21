78a51c44ec3c26d1f6b1fddf05e9b7a99079f900
ilijaaerodrom <ilija_trajkovski@outlook.com>
2022-06-21 14:23:54 +0200

    if statements

--- Youtube Courses/Bro Code/Exercise 1/main.js
+++ Youtube Courses/Bro Code/Exercise 1/main.js
@@ -101,5 +101,125 @@ let num1 = 3.14
 //num1 = Math.floor(num1);
 //num1 = Math.ceil(num1);
 //num1 = Math.pow(num1);
+//num1 = Math.sqrt(x);
 
 console.log(num1);
+
+////////////////////////////////////////////////////////////
+
+// Hypotenuse calc practise
+/*
+let a;
+let b;
+let c;
+
+a = window.prompt(`Enter side A`);
+a = Number(a);
+
+b = window.prompt(`Enter side B`);
+b = Number(b);
+
+c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+console.log(`Side c:`, c); */
+
+
+document.getElementById(`submitButton`).onclick = function() {
+
+a = document.getElementById(`aTextbox`).value;
+a = Number(a);
+
+b = document.getElementById(`bTextbox`).value;
+b = Number(b);
+
+c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+document.getElementById(`cLabel`).innerHTML = `Side C: ` + c;
+}
+
+///////////////////////////////////////////////////////////////////
+
+// Counter program
+
+let count = 0;
+
+document.getElementById(`increaseButton`).onclick = function() {
+  count -= 1;
+  document.getElementById(`countLabel`).innerHTML = count;
+}
+
+document.getElementById(`resetButton`).onclick = function() {
+  count = 0;
+  document.getElementById(`countLabel`).innerHTML = count;
+}
+
+document.getElementById(`decreaseButton`).onclick = function() {
+  count += 1;
+  document.getElementById(`countLabel`).innerHTML = count;
+}
+
+////////////////////////////////////////////////////////////////////////
+
+// Random Number Generator
+
+let firstDise;
+let seconfDise;
+let thirdDise;
+
+document.getElementById(`rollButton`).onclik = function() {
+
+   firstDise = Math.floor(Math.random() * 6) + 1;
+   seconfDise = Math.floor(Math.random() * 6) + 1;
+   thirdDise = Math.floor(Math.random() * 6) + 1;
+
+   document.getElementById(`firstLabel`).innerHTML = firstDise;
+   document.getElementById(`secondLabel`).innerHTML = secondDise;
+   document.getElementById(`thirdLabel`).innerHTML = thirdDise;
+}
+
+////////////////////////////////////////////////////////////////////////
+
+// Useful string properties & methods
+
+let myName = `   Ilija Trajkovski  `;
+let phoneNumber = `123-456-7890`;
+
+console.log(myName.length);
+console.log(myName.charAt(0));
+console.log(myName.indexOf(`T`));
+console.log(myName.lastIndexOf(`i`));
+console.log(myName.trim());
+
+phoneNumber = phoneNumber.replaceAll(`-`, `/`);
+console.log(phoneNumber);
+
+////////////////////////////////////////////////////////////////////////
+
+// Slice() extracts a section of a string
+// and returns it as a new string,
+// without modifing the original string
+
+let fullName = `Ilija Trajkovski`;
+let firstNameOfMe;
+let lastNameOfMe;
+
+//firstNameOfMe = fullName.slice(0, 5);
+//lastNameOfMe = fullName.slice(6);
+
+firstNameOfMe = fullName.slice(0, fullName.indexOf(` `));
+lastNameOfMe = fullName.slice(fullName.indexOf(` `) + 1);
+
+console.log(firstNameOfMe);
+console.log(lastNameOfMe);
+
+////////////////////////////////////////////////////////////////////////
+
+// Method chaining = caling one method after another
+//  in one continuous line of code
+
+let name = `ilija`;
+
+let letter = name.charAt(0).toUpperCase();
+
+console.log(letter);
+
+////////////////////////////////////////////////////////////////////////
